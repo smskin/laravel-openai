2e012f49d4afd6f13f0c0c5c9f40438edccc388a
Sergey Mikhailov (smskin) <sms7@yandex.ru>
2024-05-20 17:43:39 +0300

    Development

--- src/Providers/ServiceProvider.php
+++ src/Providers/ServiceProvider.php
@@ -8,6 +8,28 @@ use OpenAI;
 use OpenAI\Contracts\ClientContract;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
+use SMSkin\LaravelOpenAi\Api;
+use SMSkin\LaravelOpenAi\AssistantFileModule;
+use SMSkin\LaravelOpenAi\AssistantModule;
+use SMSkin\LaravelOpenAi\AudioModule;
+use SMSkin\LaravelOpenAi\ChatModule;
+use SMSkin\LaravelOpenAi\CompletionModule;
+use SMSkin\LaravelOpenAi\Contracts\IApi;
+use SMSkin\LaravelOpenAi\Contracts\IAssistantFileModule;
+use SMSkin\LaravelOpenAi\Contracts\IAssistantModule;
+use SMSkin\LaravelOpenAi\Contracts\IAudioModule;
+use SMSkin\LaravelOpenAi\Contracts\IChatModule;
+use SMSkin\LaravelOpenAi\Contracts\ICompletionModule;
+use SMSkin\LaravelOpenAi\Contracts\IImageModule;
+use SMSkin\LaravelOpenAi\Contracts\IThreadMessageFileModule;
+use SMSkin\LaravelOpenAi\Contracts\IThreadMessageModule;
+use SMSkin\LaravelOpenAi\Contracts\IThreadModule;
+use SMSkin\LaravelOpenAi\Contracts\IThreadRunModule;
+use SMSkin\LaravelOpenAi\ImageModule;
+use SMSkin\LaravelOpenAi\ThreadMessageFileModule;
+use SMSkin\LaravelOpenAi\ThreadMessageModule;
+use SMSkin\LaravelOpenAi\ThreadModule;
+use SMSkin\LaravelOpenAi\ThreadRunModule;
 
 class ServiceProvider extends \Illuminate\Support\ServiceProvider
 {
@@ -29,11 +51,39 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
 
     private function mapModules()
     {
-        foreach (Config::get('openai.mapper') as $interface => $class) {
-            $this->app->singleton($interface, static function () use ($class) {
-                return new $class();
-            });
-        }
+        $this->app->singleton(IApi::class, static function () {
+            return new Api();
+        });
+        $this->app->singleton(IThreadModule::class, static function () {
+            return new ThreadModule();
+        });
+        $this->app->singleton(IThreadRunModule::class, static function () {
+            return new ThreadRunModule();
+        });
+        $this->app->singleton(IThreadMessageModule::class, static function () {
+            return new ThreadMessageModule();
+        });
+        $this->app->singleton(IThreadMessageFileModule::class, static function () {
+            return new ThreadMessageFileModule();
+        });
+        $this->app->singleton(IImageModule::class, static function () {
+            return new ImageModule();
+        });
+        $this->app->singleton(ICompletionModule::class, static function () {
+            return new CompletionModule();
+        });
+        $this->app->singleton(IChatModule::class, static function () {
+            return new ChatModule();
+        });
+        $this->app->singleton(IAudioModule::class, static function () {
+            return new AudioModule();
+        });
+        $this->app->singleton(IAssistantModule::class, static function () {
+            return new AssistantModule();
+        });
+        $this->app->singleton(IAssistantFileModule::class, static function () {
+            return new AssistantFileModule();
+        });
     }
 
     private function mapApiClient()
@@ -45,7 +95,7 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
                 ->withBaseUri(Config::get('openai.client.base_uri'))
                 ->withHttpClient($client = new HttpClient([]))
                 ->withHttpHeader('OpenAI-Beta', 'assistants=v1')
-                ->withStreamHandler(static fn (RequestInterface $request): ResponseInterface => $client->send($request, [
+                ->withStreamHandler(static fn(RequestInterface $request): ResponseInterface => $client->send($request, [
                     'stream' => true,
                 ]))
                 ->make();
